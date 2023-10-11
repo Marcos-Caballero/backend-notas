cc950a603ee4150a965469ce3a365445c2634593
Marcos-Caballero <robayo1100@gmail.com>
2023-10-11 08:46:21 -0500

    vinculando db en mongo atlas intento 1

--- src/app.js
+++ src/app.js
@@ -2,24 +2,35 @@ import express from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
 import router from "./routes/notas.js";
-import cors from  "cors"
+import cors from "cors";
 
 /* raiz de la app */
 const app = express();
 /* Concexión a la db */
-mongoose.connect('mongodb://127.0.0.1:27017/notas')
-.then(() => {
-    console.log('Conectado a la base de datos')
-})
+mongoose.connect('mongodb+srv://admin:admin777@blocnotasdb.akuruqk.mongodb.net/blocnotasdb?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Conectado a la base de datos de MongoDB Atlas');
+    })
+    .catch(error => {
+        console.error('Error al conectar a MongoDB Atlas:', error);
+    });
+
 /* Configuracion */
-app.set('PORT', 5000)
+const PORT = process.env.PORT || 5000; // Elige un puerto predeterminado (5000) si no se proporciona la variable de entorno PORT.
+
 /* Middleware */
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(cors({origin: ['http://localhost:3000']}))
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(
+    cors({
+        origin: 'https://bloc-de-notas-final-sb8rh1zs8-maicol-caballeros-projects.vercel.app',
+    })
+);
+
 /* Rutas */
-app.use('/api/', router)
+app.use('/api/', router);
+
 /* Servidor arrancando */
-app.listen(app.get('PORT'), (
-    console.log("Servidor corriendo en puerto 5000")
-))
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+    });
